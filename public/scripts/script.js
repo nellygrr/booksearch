234ea76ac8c7a2045a5720c4a49910f3726dc2df
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-03-07 16:44:42 +0100

    jerem

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,49 +1,14 @@
-$(document).ready(function()// pour charger du JS "convenablement", le fichier est bien dans la balise head MAIS
-{// il faut absolument mettre un code équivalent, dans tous les cas, l'événement à surveiller s'appelle DOMContentLoaded
-	$(".navbar-toggle").click(function ()
-	{
-		$(this).toggleClass("active");
-	});
-	if ($('#search_product').length == 1 && $('#search_category').length == 1)
-	{
-		var def = $('#search_input').val();
-		$('#search_input').val('');
-		$('#search_input').focus();
-		$('#search_input').val(def);
-		$('#search_input').keyup(function()
-		{
-			var val = encodeURIComponent($(this).val());
-			history.pushState({}, "Recherche "+val, "index.php?page=search&search="+val);
-			/*
-				var req = new XMLHttpRequest();
-				req.open('GET', "index.php?page=search_product&ajax&search="+val, true);
-				req.onreadystatechange = function (aEvt)
-				{
-				  if (req.readyState == 4)
-				  {
-				     if(req.status == 200)
-				      $('#search_category').html(req.responseText);
-				     else
-				      alert("Erreur pendant le chargement de la page.\n");
-				  }
-				};
-				req.send(null);
-			*/
-			// XMLHttpRequest -> permet les requêtes AJAX
-			$('#search_product').load("index.php?page=search_product&ajax&search="+val);
-			// $('#search_category').load("index.php?page=search_category&ajax&search="+val);
-			$.get("index.php?page=search_category&ajax&search="+val, function(data)
-			{
-				alert(data);
-				$('#search_category').html(data);
-			});
-		});
-	}
-	else
-	{
-		$('#search_input').keyup(function()
-		{
-			$(this).parents('form').submit();
-		});
-	}
+$(document).ready(function()
+{
+	var name = encodeURIComponent($('#name').val());
+	var isbn = encodeURIComponent($('#isbn').val());
+	var author = encodeURIComponent($('#author').val());
+	var country = encodeURIComponent($('#country').val());
+	var gender = encodeURIComponent($('#gender').val());
+	var year1 = encodeURIComponent($('#year1').val());
+	var year2 = encodeURIComponent($('#year2').val());
+	var editorial = encodeURIComponent($('#editorial').val());
+	var price1 = encodeURIComponent($('#price1').val());
+	var price2 = encodeURIComponent($('#price2').val());
+	$('#resultats').load("index.php?page=search_elem&ajax&name="+name+"&isbn="+isbn+"&author="+author+"&country="+country+"&gender="+gender+"&year1="+year1+"&year2="+year2+"&editorial="+editorial+"&price1="+price1+"&price2="+price2);
 });
