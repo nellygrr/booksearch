1b872af990f17720ed5dda91d824171ef2c23e5d
nellygrr <neeeeelly@gmail.com>
2017-03-07 11:40:59 +0100

    apps

--- apps/search_elem.php
+++ apps/search_elem.php
@@ -2,10 +2,11 @@
 $manager = new BookManager($db);
 if (isset($_GET['name'], $_GET['author']))
 {
-	$list = $manager->search($_GET['name'], $_GET['author']);
+	$list = $manager->search($_GET['name'], $_GET['author'], $_GET['gender'], $_GET['year1'], $_GET['year2'], $_GET['editorial'], $_GET['isbn'], $_GET['price1'], $_GET['price2']);
 	foreach ($list AS $book)
 	{
 		require('views/search_elem.phtml');
 	}
+	var_dump($_GET);
 }
 ?>
