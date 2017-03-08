b0622545f28459ad7828c9a91c1d1f21b5567c24
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-03-08 11:16:51 +0100

    jerem

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-	$('.search-input', '.search-input-numbers').keyup(function()
+	$('.search-input, .search-input-numbers').keyup(function()
 	{
 		var name = encodeURIComponent($('#name').val());
 		var isbn = encodeURIComponent($('#isbn').val());
@@ -9,9 +9,10 @@ $(document).ready(function()
 		var gender = encodeURIComponent($('#gender').val());
 		var year1 = encodeURIComponent($('#year1').val());
 		var year2 = encodeURIComponent($('#year2').val());
-		var editorial = encgit odeURIComponent($('#editorial').val());
+		var editorial = encodeURIComponent($('#editorial').val());
 		var price1 = encodeURIComponent($('#price1').val());
 		var price2 = encodeURIComponent($('#price2').val());
 		$('#resultats').load("index.php?page=search_elem&ajax&name="+name+"&isbn="+isbn+"&author="+author+"&country="+country+"&gender="+gender+"&year1="+year1+"&year2="+year2+"&editorial="+editorial+"&price1="+price1+"&price2="+price2);
+		alert("index.php?page=search_elem&ajax&name="+name+"&isbn="+isbn+"&author="+author+"&country="+country+"&gender="+gender+"&year1="+year1+"&year2="+year2+"&editorial="+editorial+"&price1="+price1+"&price2="+price2)
 	});
 });
