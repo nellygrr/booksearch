683cd2fcb712411c9e7fe7a5dbd37498a41b8342
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-03-08 11:24:34 +0100

    jerem

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,6 +13,5 @@ $(document).ready(function()
 		var price1 = encodeURIComponent($('#price1').val());
 		var price2 = encodeURIComponent($('#price2').val());
 		$('#resultats').load("index.php?page=search_elem&ajax&name="+name+"&isbn="+isbn+"&author="+author+"&country="+country+"&gender="+gender+"&year1="+year1+"&year2="+year2+"&editorial="+editorial+"&price1="+price1+"&price2="+price2);
-		alert("index.php?page=search_elem&ajax&name="+name+"&isbn="+isbn+"&author="+author+"&country="+country+"&gender="+gender+"&year1="+year1+"&year2="+year2+"&editorial="+editorial+"&price1="+price1+"&price2="+price2)
 	});
 });
